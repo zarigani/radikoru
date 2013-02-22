38f54f61b9972fe901deaa80053d835ed5f8adfa
zariganitosh <XXXX@example.com>
2013-02-22 14:18:08 +0900

    Modify install.sh "mv" to "cp -p".

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@ INSTALL_DIRECTORY=${1:-/usr/local/bin}
 [ $# = 0 ] && mkdir -p /usr/local/bin
 
 cd `dirname $0`
-mv rec_radiko.sh $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/rec_radiko.sh
-mv rec_radiru.sh $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/rec_radiru.sh
-mv rec_radikoru.sh $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/rec_radikoru.sh
+cp -p rec_radiko.sh $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/rec_radiko.sh
+cp -p rec_radiru.sh $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/rec_radiru.sh
+cp -p rec_radikoru.sh $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/rec_radikoru.sh
+cp -p find_radikoru.sh $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/rec_radikoru.sh
 
 cd $INSTALL_DIRECTORY
-chmod +x rec_radiko.sh rec_radiru.sh rec_radikoru.sh
+chmod +x rec_radiko.sh rec_radiru.sh rec_radikoru.sh find_radikoru.sh
