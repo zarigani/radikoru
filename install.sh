523e9867179a1c05d2af57f9c1aa7df9481d3596
zariganitosh <XXXX@example.com>
2013-10-15 14:16:31 +0900

    fix file's name in install.sh.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 cp -p rec_radiko.sh $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/rec_radiko.sh
 cp -p rec_radiru.sh $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/rec_radiru.sh
 cp -p rec_radikoru.sh $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/rec_radikoru.sh
-cp -p find_radikoru.sh $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/rec_radikoru.sh
+cp -p find_radiko.sh $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/find_radiko.sh
 
 cd $INSTALL_DIRECTORY
-chmod +x rec_radiko.sh rec_radiru.sh rec_radikoru.sh find_radikoru.sh
+chmod +x rec_radiko.sh rec_radiru.sh rec_radikoru.sh find_radiko.sh
